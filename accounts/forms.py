7dc83ebf231225ffc33936cc19e0e1a3eebc6485
daniarlert <arlertdaniel@gmail.com>
2023-07-06 21:14:11 +0200

    feat(accounts): add str method to custom user

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,10 +1,10 @@
+from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = (
             "username",
             "email",
@@ -13,7 +13,7 @@ class CustomUserCreationForm(UserCreationForm):
 
 class CustomUserChangeForm(UserChangeForm):
     class Meta:
-        model = CustomUser
+        model = get_user_model()
         fields = (
             "username",
             "email",
