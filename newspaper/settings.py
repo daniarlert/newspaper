18a3dd11ba983c2ac592092d05e9b0b018006bff
daniarlert <arlertdaniel@gmail.com>
2023-07-07 12:57:21 +0200

    feat: handle static assets

--- newspaper/settings.py
+++ newspaper/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
 LOCAL_APPS = [
     "accounts",
     "pages",
+    "articles",
 ]
 
 INSTALLED_APPS += LOCAL_APPS
@@ -119,6 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- newspaper/urls.py
+++ newspaper/urls.py
@@ -5,8 +5,12 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include("pages.urls")),
-    path("accounts/", include("django.contrib.auth.urls")),
-    path("accounts/", include("accounts.urls")),
+    # Django admin
     path("admin/", admin.site.urls),
+    # User management
+    path("accounts/", include("django.contrib.auth.urls")),
+    # Local apps
+    path("accounts/", include("accounts.urls")),
+    path("articles/", include("articles.urls")),
+    path("", include("pages.urls")),
 ]
