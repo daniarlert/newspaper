360648066b4ecbe136e5de7385fdbe9bf60f4671
daniarlert <arlertdaniel@gmail.com>
2023-07-13 13:22:40 +0200

    feat(articles): add comments

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,5 @@
 import uuid
-from django.conf import settings
+from django.contrib.auth import get_user_model
 from django.db import models
 from django.urls import reverse
 
@@ -14,7 +14,7 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
+        get_user_model(),
         on_delete=models.CASCADE,
     )
 
@@ -23,3 +23,19 @@ class Article(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", args=[str(self.id)])
+
+
+class Comment(models.Model):
+    article = models.ForeignKey(
+        Article,
+        on_delete=models.CASCADE,
+        related_name="comments",
+    )
+    comment = models.CharField(max_length=255)
+    author = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self) -> str:
+        return self.comment
